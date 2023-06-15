23bc5cef235909d311bb7ad5acb0a92b65a6f29c
Rita Brilhante <meritabrilhante@gmail.com>
2023-06-15 18:27:21 -0300

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import '../styles/global.scss'
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis debitis in ullam ratione eveniet, delectus minus dolore optio nemo iste, minima neque eligendi quisquam nulla et, recusandae alias! Mollitia, nisi!</h1>
+    </main>
+  )
+}
